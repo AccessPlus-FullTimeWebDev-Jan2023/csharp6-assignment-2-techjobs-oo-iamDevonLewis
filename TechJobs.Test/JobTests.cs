015ba24851ff397548cd08335d2329eeadf7c0a9
Devon Lewis <devonlewis_23@yahoo.com>
2023-04-03 04:10:14 -0500

    task 4 partially completed

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -6,11 +6,38 @@ namespace TechJobs.Tests
 	{
         //Testing Objects
         //initalize your testing objects here
+        Job job1 = new Job();
+
+        Job job2 = new Job();
+
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
         [TestMethod]
-        public void TestMethod()
+        public void TestSettingJobId()
         {
-            //TODO: Task 4: remove this method before you create your first test method 
+            //Assert.AreEqual(1, job1.Id);
+            //Assert.AreEqual(2,job2.Id);
+            Assert.IsFalse(job1.Id.Equals(job2.Id));
+            
+            
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Assert.AreEqual("Product tester", job3.Name);
+            Assert.AreEqual("ACME", job3.EmployerName.Value);
+            Assert.AreEqual("Desert", job3.EmployerLocation.Value);
+            Assert.AreEqual("Quality control", job3.JobType.Value);
+            Assert.AreEqual("Persistence", job3.JobCoreCompetency.Value);
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Assert.IsFalse(job1.Id.Equals(job2.Id));
         }
     }
 }
